10816eb8824f1b59bf705077a4f2d73c9eaa9e65
hahehihoh <102732246+hahehihoh@users.noreply.github.com>
2022-05-03 09:56:57 +0900

    Day 1

---
+++ Day_01.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Hello World      ')
+
+
+# In[232]:
+
+
+a = 5
+b = 7
+c = a+b
+c
+
+
+# # 오늘의 주제
+#  * 파이썬 인터프리터의 이해
+#  * Jupyter notebook 활용하기
+#  * 주석문의 종류
+
+# In[235]:
+
+
+msg = "Hello"
+print(msg)
+
+msg = "World"
+print(msg)
+
+msg = """This
+        is
+        my
+        code"""
+print(msg)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# dd는 삭제 b는 추가 a는 위에 추가
+
